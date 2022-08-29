1aedc6d7d19e5d0e2f5fd96d8fc4205391ce0636
benjamin perrin <perrin.benjamin77@gmail.com>
2022-08-29 12:37:01 +0200

    setting dev/prod env

---
+++ src/main/java/fr/m2i/javaspring/config/ProdDataSourceConfig.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.m2i.javaspring.config;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author ben
+ */
+@Component
+@Profile("prod")
+public class ProdDataSourceConfig implements DataSourceConfig{
+   
+    @Override
+    public void setup(){
+        System.out.println("setting up datasource for prod env");
+    }
+}
